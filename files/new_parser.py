ffc7e3c918b27224c12d3bdda92b0a08488e8ccf
KaptainMidnight <alexartemkin173@mail.ru>
2019-03-11 18:42:26 +0400

    sas

--- files/new_parser.py
+++ files/new_parser.py
@@ -22,7 +22,6 @@ def selenium_get_href(b_url):
     driver = wd.Chrome(chromedriver)
     driver.get(b_url)
     # Все что будем брать из главного меню
-    ads = driver.find_elements_by_class_name("_328WR")  # Ищем все объявления
     city = driver.find_element_by_class_name("_20Ixl")  # Ищем город в ссылках
 
     SCROLL_PAUSE_TIME = 0.5
@@ -31,33 +30,39 @@ def selenium_get_href(b_url):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(SCROLL_PAUSE_TIME)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        if driver.find_element_by_class_name("DnHhI").is_displayed():
-            button = driver.find_element_by_class_name("_2bexo")
-            time.sleep(0.5)
-            button.click()
+        try:
+            if driver.find_element_by_class_name("DnHhI").is_displayed():
+                button = driver.find_element_by_class_name("_2bexo").click()
+                time.sleep(SCROLL_PAUSE_TIME)
+                href = driver.find_elements_by_class_name("MBUbs")  # Ищем ссылки на объявления
+                for j in href:
+                    links = {
+                        "href": j.get_attribute("href")
+                    }
+                    if links not in urls:
+                        urls.append(links)
+                        print(len(urls))
+        except:
+            continue
+
+
+def parse_avito(href):
+    for data in href:
+        session = requests.Session()
+        response = session.get(data["href"])
+        if response.status_code == 200:
+            soup = bs(response.content, "lxml")
+            contact_div = soup.find_all("div", attrs={"class": "_3U_HU"})
+            for contact in contact_div:
+                try:
+                    name = contact.find("span", attrs={"class": "ZvfUX"}).text
+                    phone = contact.find("span", attrs={"class": "_2MOUQ"})['href']
+                    print(name, phone)
+                except:
+                    continue
         else:
-            pass
-
-    # href = driver.find_elements_by_class_name("MBUbs")  # Ищем ссылки на объявления
-    # for j in href:
-    #     links = {
-    #         "href": j.get_attribute("href")
-    #     }
-    #     urls.append(len(links))
-    #     print(len(urls))
-    # for data in users:
-    #     session = requests.Session()
-    #     response = session.get(data["href"])
-    #     if response.status_code == 200:
-    #         soup = bs(response.content, "lxml")
-    #         contact_div = soup.find_all("div", attrs={"class": "_3U_HU"})
-    #         for contact in contact_div:
-    #             name = contact.find("span", attrs={"class": "ZvfUX"}).text
-    #             phone = contact.find("span", attrs={"class": "_2MOUQ"}).get("href")
-    #             print(name, phone)
-    #     else:
-    #         print("ERROR")
-    driver.quit()
+            print("ERROR")
 
 
-selenium_get_href("https://m.avito.ru/rossiya/nedvizhimost")
+html = selenium_get_href("https://m.avito.ru/rossiya/dlya_biznesa")
+parse_avito(html)
